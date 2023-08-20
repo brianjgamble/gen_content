57b66f8b92ebd0bd966e4fbdf996957bd7b38a2e
Brian Gamble <brianjgamble@users.noreply.github.com>
2023-08-20 11:38:22 -0500

    Add tags to the content struct

--- lib/gen_content/content.ex
+++ lib/gen_content/content.ex
@@ -1,12 +1,13 @@
 defmodule GenContent.Content do
   @moduledoc false
 
-  defstruct slug: "", title: "", date: "", text: ""
+  defstruct slug: "", title: "", date: "", text: "", tags: []
 
   @type t :: %__MODULE__{
-          slug: binary(),
-          title: binary(),
-          date: binary(),
-          text: binary()
+          slug: String.t(),
+          title: String.t(),
+          date: String.t(),
+          text: String.t(),
+          tags: List.t()
         }
 end
